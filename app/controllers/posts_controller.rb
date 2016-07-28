b86f57e8280cf320294414c0320bf5c3c630b3a4
Shin <mrmaxin@gmail.com>
2016-07-28 16:51:18 -0700

    add post resource to routes.rb and actions to posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+
+    if @post.save
+      redirect_to @post, notice: "Post created"
+    else
+      render 'new', notice: "Unable to save post"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :date, :content)
+  end
 end
