dc873cd59bded25419ce0ec41d160f0b8f42aaf3
Shin <mrmaxin@gmail.com>
2016-07-28 17:19:17 -0700

    add edit upgrade destroy to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -16,9 +18,33 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update post_params
+      redirect_to @post, notice: "updated post"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :date, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
