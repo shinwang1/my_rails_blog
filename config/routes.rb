76ade92afdd74f2abf4986e1a78859cad512e109
Shin <mrmaxin@gmail.com>
2016-09-29 15:43:24 -0700

    add command to manifest.yml to fix error to otherwise not migrated models

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :projects
   resources :contacts, only: [:new, :create]
-  get 'welcome/index'
+  #get 'welcome/index'
   root 'welcome#index'
 
   get '*path' => redirect('/')
